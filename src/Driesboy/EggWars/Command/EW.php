eaa900757f05957dde8efe93759ace7c7827cb15
MC-Nation <herojhjh@outlook.com>
2017-06-27 01:06:45 +0700

    FloatTag.... (#45)

--- src/Driesboy/EggWars/Command/EW.php
+++ src/Driesboy/EggWars/Command/EW.php
@@ -11,6 +11,7 @@ use pocketmine\nbt\tag\ByteTag;
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\nbt\tag\DoubleTag;
 use pocketmine\nbt\tag\ListTag;
+use pocketmine\nbt\tag\FloatTag;
 use pocketmine\nbt\tag\ShortTag;
 use pocketmine\nbt\tag\StringTag;
 use pocketmine\Player;
